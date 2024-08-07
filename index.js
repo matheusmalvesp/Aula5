6b759b72b154e03de8dab4f3e5b5ee1cf3c7f781
MM Alves <95140189+matheusmalvesp@users.noreply.github.com>
2024-08-07 20:06:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const porta = 443
+const express = require('express');
+const app = express();
+const porta = 443;
 
 app.get('/', (req, res) => {
-  res.send('Olá, Mundo! Estou usando o express!')
+  res.send('Olá, Mundo! Estou usando o express!');
 })
 
-app.listen(porta, () => {console.log('App rodando')})
+app.listen(porta, () => {console.log('App rodando')});
