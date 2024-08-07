3a36caf03abf4092de8660c32c0d60fcd808dbd2
MM Alves <calendariousfterravermelha@gmail.com>
2024-08-07 19:41:36 -0300

    Upload para teste de API

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const porta = 443
+
+app.get('/', (req, res) => {
+  res.send('Olá, Mundo! Estou usando o express!')
+})
+
+app.listen(porta, () => {console.log('App rodando')})
